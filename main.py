1ba575859fbe4765cb2af2613fa910655bda107e
Sarvar <sabdumutolibov@gmail.com>
2024-05-03 00:01:58 +0500

    'hw17'

---
+++ main.py
@@ -0,0 +1,45 @@
+#
+# #task1!
+# def arrBetween(son1, son2, lst,):
+#     lst1 = []
+#     for x in lst:
+#         if son1 < x < son2:
+#             lst1.append(x)
+#     return lst1
+#
+# print(arrBetween(3,8, [1,5,95,0,4,7]))
+# print(arrBetween(1,10, [1,10,25,8,11,6]))
+# print(arrBetween(7,32, [1,2,3,78]))
+#
+# #task2!
+# def incrementItems(lst):
+#     lst1 = []
+#     for x in lst:
+#         lst1.append(x+1)
+#     return lst1
+#
+# print(incrementItems([1,2,3]))
+# print(incrementItems([2,4,6,8]))
+# print(incrementItems([-1,-2,-3,-4]))
+#
+# #task3!
+# def calcAge(year):
+#     return year * 365
+#
+# print(calcAge(65))
+# print(calcAge(0))
+# print(calcAge(20))
+#
+# #task4!
+# def is_in_range(num, range_dict):
+#     if range_dict["min"] <= num and num <= range_dict["max"]:
+#         return True
+#     return False
+#
+# print(is_in_range(4,{"min": 0, "max": 5}))
+# print(is_in_range(4, {"min": 4, "max": 10}))
+# print(is_in_range(4, {"min": 6, "max": 5}))
+# print(is_in_range(5, {"min": 5, "max": 5}))
+
+
+
